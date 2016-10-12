236a99aff0abc7c2fabfde4e6e01d13327063b48
Yusuf Simonson <simonson@gmail.com>
2016-10-12 19:19:06 -0400

    Abstracted out commonly useful methods from the test script runner

--- support/test_scripts.py
+++ support/test_scripts.py
@@ -4,12 +4,9 @@ import os
 import requests
 import glob
 import re
-import random
 import json
 import sys
-import string
-import time
-from base64 import b64encode
+import util
 import subprocess
 from contextlib import contextmanager
 
@@ -27,10 +24,6 @@ RUNTIME_ERROR_EXPECTED_PATTERN = re.compile("-- error: runtime")
 # If a test script has this pattern, the script is expected to return an OK
 # response
 OK_EXPECTED_PATTERN = re.compile("-- ok: (.+)$")
-
-ACCOUNT_ID_MATCHER = re.compile("Account ID: (\d+)")
-
-ACCOUNT_SECRET_MATCHER = re.compile("Account secret: (.+)")
 
 class Result(object):
     """Represents a test run result"""
@@ -50,37 +43,6 @@ class Result(object):
         self.passed = passed
         self.status_code = res.status_code
         self.body = body
-
-@contextmanager
-def server():
-    """
-    Context manager for running the server. This starts the server up, waits
-    until its responsive, then yields. When the context manager's execution is
-    resumed, it kills the server.
-    """
-
-    # Start the process
-    server_proc = subprocess.Popen(["./bin/target/debug/nutrino-server"])
-
-    while True:
-        # Check if the server is now responding to HTTP requests
-        try:
-            res = requests.get("http://localhost:8000", timeout=1)
-
-            if res.status_code == 401:
-                break
-        except requests.exceptions.RequestException:
-            pass
-
-        # Server is not yet responding to HTTP requests - let's make sure it's
-        # running in the first place
-        if server_proc.poll() != None:
-            raise Exception("Server failed to start")
-
-        time.sleep(1)
-
-    yield
-    server_proc.terminate()
 
 def run_test(name, contents, user_id, secret):
     """Runs a given test"""
@@ -125,14 +87,8 @@ def run_test(name, contents, user_id, secret):
 def main(pattern="*.lua"):
     has_failure = False
 
-    random_value = "".join(random.sample(string.ascii_letters + string.digits, 10))
-    email = "test-script-runner-%s@nutrino.com" % random_value
-    create_user_output = subprocess.check_output(["./bin/target/debug/nutrino-user", "add", email]).decode("utf-8")
-    user_id = ACCOUNT_ID_MATCHER.search(create_user_output).groups()[0]
-    secret = ACCOUNT_SECRET_MATCHER.search(create_user_output).groups()[0]
-
-    try:
-        with server():
+    with util.user() as (email, user_id, secret):
+        with util.server():
             for name in glob.glob(os.path.join("bin/support/test_scripts/", pattern)):
                 with open(name, "r") as f:
                     contents = f.read()
@@ -147,8 +103,6 @@ def main(pattern="*.lua"):
                     print("=> Status code: %s" % result.status_code)
                     print("=> Response json: %s" % result.body)
                     has_failure = True
-    finally:
-        subprocess.check_output(["./bin/target/debug/nutrino-user", "remove", user_id])
 
     if has_failure:
         sys.exit(1)
