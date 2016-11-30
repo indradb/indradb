df5c0bee0c6afcb4af4bca4ecf8c0610d6d85ffe
Yusuf Simonson <simonson@gmail.com>
2016-11-29 22:30:27 -0500

    Further documentation for the datastore/transaction traits

--- src/datastore/traits.rs
+++ src/datastore/traits.rs
@@ -8,6 +8,9 @@ use chrono::naive::datetime::NaiveDateTime;
 
 /// Specifies a datastore implementation.
 ///
+/// Datastores are responsible for managing accounts, and constructing new
+/// transactions.
+///
 /// # Errors
 /// All methods may return an error if something unexpected happeneds - e.g.
 /// if there was a problem connecting to the underlying database. 
@@ -31,7 +34,18 @@ pub trait Datastore<T: Transaction<I>, I: Id> {
 	fn transaction(&self, account_id: I) -> Result<T, Error>;
 }
 
-/// Specifies a transaction implementation, which are returned by datastores
+/// Specifies a transaction implementation, which are returned by datastores.
+/// Transactions are responsible for managing the following kinds of data:
+/// 1. Vertices.
+/// 2. Edges, which connect two vertices.
+/// 3. Global metadata: metadata that is not owned by anything, and as a
+///    result needs to be manually managed.
+/// 4. Account metadata: metadata that is owned by an account, and will be
+///    automatically deleted when the associated account is deleted.
+/// 5. Vertex metadata: metadata that is owned by a vertex, and will be
+///    automatically deleted when the associated vertex is deleted.   
+/// 6. Edge metadata: metadata that is owned by an edge, and will be
+///    automatically deleted when the associated edge is deleted.   
 pub trait Transaction<I: Id> {
 	/// Gets a vertex.
 	///
@@ -79,47 +93,88 @@ pub trait Transaction<I: Id> {
 	fn delete_edge(&self, I, models::Type, I) -> Result<(), Error>;
 
 	/// Gets the number of outbound edges of a given type.
+	///
+	/// Note that this will not error out if the vertex does not exist - it
+	/// will just return 0.
 	fn get_edge_count(&self, I, models::Type) -> Result<u64, Error>;
 
-	/// Gets a range of the outbound edges of a given type. Edges should
-	/// generally be ordered by their update/creation datetime (descending),
-	/// but order may not be exact depending on the implementation. 
+	/// Gets a range of the outbound edges of a given type.
+	///
+	/// Edges should generally be ordered by their update/creation datetime
+	/// (descending), but order may not be exact depending on the
+	/// implementation. Note that this will not error out if the vertex does
+	/// not exist - it will just return an empty edge range.
 	fn get_edge_range(&self, I, models::Type, u64, u16) -> Result<Vec<models::Edge<I>>, Error>;
 
 	/// Gets a range of the outbound edges of a given type, optionally bounded
 	/// by the specified update/creation datetime upper and lower bounds.
+	///
 	/// Edges should generally be ordered by their update/creation datetime
-	/// (descending), but order may not be exact depending on the implementation. 
+	/// (descending), but order may not be exact depending on the
+	/// implementation. Note that this will not error out if the vertex does
+	/// not exist - it will just return an empty edge range.
 	fn get_edge_time_range(&self, I, models::Type, Option<NaiveDateTime>, Option<NaiveDateTime>, u16) -> Result<Vec<models::Edge<I>>, Error>;
 
 	/// Gets the number of inbound edges of a given type.
+	///
+	/// Note that this will not error out if the vertex does not exist - it
+	/// will just return 0.
 	fn get_reversed_edge_count(&self, I, models::Type) -> Result<u64, Error>;
 
-	/// Gets a range of the inbound edges of a given type. Edges should
-	/// generally be ordered by their update/creation datetime (descending),
-	/// but order may not be exact depending on the implementation. 
+	/// Gets a range of the inbound edges of a given type.
+	///
+	/// Edges should generally be ordered by their update/creation datetime
+	/// (descending), but order may not be exact depending on the
+	/// implementation. Note that this will not error out if the vertex does
+	/// not exist - it will just return an empty edge range.
 	fn get_reversed_edge_range(&self, I, models::Type, u64, u16) -> Result<Vec<models::Edge<I>>, Error>;
 
 	/// Gets a range of the inbound edges of a given type, optionally bounded
 	/// by the specified update/creation datetime upper and lower bounds.
+	///
 	/// Edges should generally be ordered by their update/creation datetime
-	/// (descending), but order may not be exact depending on the implementation. 
+	/// (descending), but order may not be exact depending on the
+	/// implementation. Note that this will not error out if the vertex does
+	/// not exist - it will just return an empty edge range.
 	fn get_reversed_edge_time_range(&self, I, models::Type, Option<NaiveDateTime>, Option<NaiveDateTime>, u16) -> Result<Vec<models::Edge<I>>, Error>;
 
+	/// Gets a global metadata value.
+	///
+	/// # Errors
+	/// Returns `Error::MetadataNotFound` if the metadata does not exist.
 	fn get_global_metadata(&self, String) -> Result<JsonValue, Error>;
+
+	/// Sets a global metadata value.
 	fn set_global_metadata(&self, String, JsonValue) -> Result<(), Error>;
+
+	/// Deletes a global metadata value.
 	fn delete_global_metadata(&self, String) -> Result<(), Error>;
 
+	/// Gets an account metadata value.
 	fn get_account_metadata(&self, I, String) -> Result<JsonValue, Error>;
+
+	/// Sets an account metadata value.
 	fn set_account_metadata(&self, I, String, JsonValue) -> Result<(), Error>;
+
+	/// Deletes an account metadata value.
 	fn delete_account_metadata(&self, I, String) -> Result<(), Error>;
 
+	/// Gets a vertex metadata value.
 	fn get_vertex_metadata(&self, I, String) -> Result<JsonValue, Error>;
+
+	/// Sets a vertex metadata value.
 	fn set_vertex_metadata(&self, I, String, JsonValue) -> Result<(), Error>;
+
+	/// Deletes a vertex metadata value.
 	fn delete_vertex_metadata(&self, I, String) -> Result<(), Error>;
 
+	/// Gets an edge metadata value.
 	fn get_edge_metadata(&self, I, models::Type, I, String) -> Result<JsonValue, Error>;
+
+	/// Sets an edge metadata value.
 	fn set_edge_metadata(&self, I, models::Type, I, String, JsonValue) -> Result<(), Error>;
+
+	/// Deletes an edge metadata value.
 	fn delete_edge_metadata(&self, I, models::Type, I, String) -> Result<(), Error>;
 
 	/// Commits the transaction.
