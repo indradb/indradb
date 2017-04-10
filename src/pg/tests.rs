43e799556de20b2e237614046dd027b9dc5c8553
Yusuf Simonson <simonson@gmail.com>
2017-04-10 00:25:01 -0400

    Some environment variable tweaks: * Removed test environment variables that are not used * Generate unique temporary directories to allow multiple braid tests to be run in parallel

--- src/pg/tests.rs
+++ src/pg/tests.rs
@@ -8,7 +8,7 @@ use std::sync::{Once, ONCE_INIT};
 static START: Once = ONCE_INIT;
 
 fn datastore() -> PostgresDatastore {
-    let connection_string = env::var("DATABASE_URL").expect("Expected a DATABASE_URL");
+    let connection_string = env::var("TEST_POSTGRES_URL").expect("Expected a TEST_POSTGRES_URL");
 
     START.call_once(|| {
         PostgresDatastore::create_schema(connection_string.clone()).unwrap();
--- test.sh
+++ test.sh
@@ -2,12 +2,10 @@
 
 set -e
 
-export DATABASE_NAME=braid_test
-export TEST_RDB_DIRECTORY="/tmp/test-rdb"
+export TEST_RDB_DIRECTORY=`mktemp -d 2>/dev/null || mktemp -d -t 'braid'`
 export RUST_BACKTRACE=1
 export SECRET=QkrDxgVJCT
-export DATABASE_URL=postgres://postgres@localhost:5432/braid_test
-export BRAID_SCRIPT_ROOT=`pwd`/bin/test_scripts
+export TEST_POSTGRES_URL=postgres://postgres@localhost:5432/braid_test
 
 function cleanup {
     echo "Cleaning up..."
@@ -17,6 +15,6 @@ function cleanup {
 mkdir -p $TEST_RDB_DIRECTORY
 trap cleanup EXIT
 
-dropdb --if-exists $DATABASE_NAME
-createdb --owner=postgres $DATABASE_NAME
+dropdb --if-exists braid_test
+createdb --owner=postgres braid_test
 cargo test $TEST_NAME
