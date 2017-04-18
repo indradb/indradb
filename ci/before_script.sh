1979e106b1166072d18801434495d3c01dca8663
Yusuf Simonson <simonson@gmail.com>
2017-04-17 22:12:04 -0400

    Wait until postgres is started before continuing

--- ci/before_script.sh
+++ ci/before_script.sh
@@ -3,7 +3,7 @@
 if [ $TRAVIS_OS_NAME = osx ]; then
     rm -rf /usr/local/var/postgres
     initdb /usr/local/var/postgres
-    pg_ctl -D /usr/local/var/postgres start
+    pg_ctl -D /usr/local/var/postgres -w start
     createuser -s postgres
 fi
 
