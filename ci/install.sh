8d981a890301bc124709255f599d87cf8164036d
Yusuf Simonson <simonson@gmail.com>
2017-04-17 21:01:49 -0400

    Brew install packages

--- ci/install.sh
+++ ci/install.sh
@@ -22,6 +22,16 @@ main() {
            --git japaric/cross \
            --tag $tag \
            --target $target
+
+    if [ $TRAVIS_OS_NAME = linux ]; then
+        sudo apt-get -qq update
+        sudo apt-get install -y lua5.1 liblua5.1-0-dev
+    else
+        brew update
+        brew install lua@5.1
+    fi
+
+    source ~/.cargo/env || true
 }
 
 main
