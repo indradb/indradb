dab965e354c2ac17b89d7997100bd0bfcb5c09ad
Jason Feinstein <jason.feinstein@gmail.com>
2017-05-07 21:21:17 +0200

    Use CAPITAL_LETTERS_WITH_UNDERSCORES for bash vars

--- test.sh
+++ test.sh
@@ -2,21 +2,21 @@
 
 set -e
 
-postgresuser='postgres'
-unamestr=`uname`
-if [[ "$unamestr" == 'Darwin' ]]; then
-    postgresuser=`whoami`
-    echo "Using user '${postgresuser}' for postgres" 
+PG_USER='postgres'
+UNAME_STR=`uname`
+if [[ "$UNAME_STR" == 'Darwin' ]]; then
+    PG_USER=`whoami`
+    echo "Using user '${PG_USER}' for postgres" 
 fi
 
 export DATABASE_NAME=braid_test
 export RUST_BACKTRACE=1
 export SECRET=QkrDxgVJCT
-export DATABASE_URL="postgres://${postgresuser}@localhost:5432/braid_test"
+export DATABASE_URL="postgres://${PG_USER}@localhost:5432/braid_test"
 export BRAID_SCRIPT_ROOT=`pwd`/test_scripts
 
 dropdb --if-exists braid_test
-createdb --owner=$postgresuser braid_test
+createdb --owner=$PG_USER braid_test
 cargo build
 ./target/debug/braid-db init
 ./support/with_server.sh cargo test $TEST_NAME
