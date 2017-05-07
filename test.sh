f0024a78512c8b4388f88cff6c27e52683986bd2
Jason Feinstein <jason.feinstein@gmail.com>
2017-05-07 21:08:13 +0200

    Update test.sh to detect MacOS
    When the host OS is MacOS, use the current user as the database user for
    postgres connections.

--- test.sh
+++ test.sh
@@ -2,14 +2,22 @@
 
 set -e
 
+postgresuser='postgres'
+unamestr=`uname`
+if [[ "$unamestr" == 'Darwin' ]]; then
+    # use the current user if the
+    postgresuser=`whoami`
+    echo "Using user '${postgresuser}' for postgres" 
+fi
+
 export DATABASE_NAME=braid_test
 export RUST_BACKTRACE=1
 export SECRET=QkrDxgVJCT
-export DATABASE_URL=postgres://postgres@localhost:5432/braid_test
+export DATABASE_URL="postgres://${postgresuser}@localhost:5432/braid_test"
 export BRAID_SCRIPT_ROOT=`pwd`/test_scripts
 
 dropdb --if-exists braid_test
-createdb --owner=postgres braid_test
+createdb --owner=$postgresuser braid_test
 cargo build
 ./target/debug/braid-db init
 ./support/with_server.sh cargo test $TEST_NAME
