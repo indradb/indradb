acc5147ff681174c3316c98e403924846650e3f8
Jason Feinstein <jason.feinstein@gmail.com>
2017-05-07 21:58:11 +0200

    Update test.sh to work on MacOS

--- test.sh
+++ test.sh
@@ -2,18 +2,25 @@
 
 set -e
 
+PG_USER='postgres'
+UNAME_STR=`uname`
+if [[ "$UNAME_STR" == 'Darwin' ]]; then
+    PG_USER=`whoami`
+    echo "Using user '${PG_USER}' for postgres"
+fi
+
 export TEST_RDB_DIRECTORY=`mktemp -d 2>/dev/null || mktemp -d -t 'braid'`
 export RUST_BACKTRACE=1
 export SECRET=QkrDxgVJCT
-export TEST_POSTGRES_URL=postgres://postgres@localhost:5432/braid_test
+export TEST_POSTGRES_URL="postgres://${PG_USER}@localhost:5432/braid_test"
 
 ACTION=test
 
 while true; do
-  case "$1" in
-    --bench) ACTION=bench; shift ;;
-    * ) break ;;
-  esac
+    case "$1" in
+        --bench) ACTION=bench; shift ;;
+        * ) break ;;
+    esac
 done
 
 function cleanup {
@@ -25,5 +32,5 @@ mkdir -p $TEST_RDB_DIRECTORY
 trap cleanup EXIT
 
 dropdb --if-exists braid_test
-createdb --owner=postgres braid_test
+createdb --owner=$PG_USER braid_test
 cargo $ACTION $TEST_NAME
