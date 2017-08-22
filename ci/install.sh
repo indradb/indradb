62fa56c3086cd18c85e4d91f211dcac2d0706bea
Yusuf Simonson <simonson@gmail.com>
2017-08-21 23:09:11 -0400

    TravisCI: Removed workaround for rustdoc failing on osx

--- ci/install.sh
+++ ci/install.sh
@@ -8,21 +8,7 @@ main() {
         sudo apt-get install -y lua5.1 liblua5.1-0-dev
     else
         brew update
-
-        # TODO: Currently `rustdoc` fails on osx/rust stable due to an issue
-        # with `libjpeg`. Once it's fixed, remove the manually tweaking here.
-        brew install lua@5.1 libjpeg
-        brew link libjpeg
-        pushd /usr/local/lib
-            rm libgif.dylib
-            ln -s /System/Library/Frameworks/ImageIO.framework/Resources/libGIF.dylib libGIF.dylib
-            rm libjpeg.dylib
-            ln -s /System/Library/Frameworks/ImageIO.framework/Resources/libJPEG.dylib libJPEG.dylib
-            rm libtiff.dylib
-            ln -s /System/Library/Frameworks/ImageIO.framework/Resources/libTIFF.dylib libTIFF.dylib
-            rm libpng.dylib
-            ln -s /System/Library/Frameworks/ImageIO.framework/Resources/libPng.dylib libPng.dylib
-        popd
+        brew install lua@5.1
     fi
 
     source ~/.cargo/env || true
