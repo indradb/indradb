d7a3b05a5df4c8765ab8bb01c90df0e0853bd9b1
Yusuf Simonson <simonson@gmail.com>
2018-04-12 16:45:12 +0800

    Autobuild gRPC files

---
+++ bin/build.rs
@@ -0,0 +1,17 @@
+extern crate protoc_grpcio;
+
+fn main() {
+    protoc_grpcio::compile_grpc_protos(
+        &[
+            "edges.proto",
+            "metadata.proto",
+            "queries.proto",
+            "request.proto",
+            "response.proto",
+            "service.proto",
+            "vertices.proto",
+        ],
+        &["proto"],
+        "src/server/autogen"
+    ).expect("failed to compile gRPC definitions");
+}
--- bin/test.sh
+++ bin/test.sh
@@ -3,7 +3,6 @@
 set -e
 
 export RUST_BACKTRACE=1
-export INDRADB_SCRIPT_ROOT=`pwd`/test_scripts
 
 cargo build
 cargo test $TEST_NAME
