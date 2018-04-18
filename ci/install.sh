e2836783f76717d29c7cc7f234f4d3282d186097
Yusuf Simonson <simonson@gmail.com>
2018-04-18 13:07:44 +0700

    Fixed paths

--- ci/install.sh
+++ ci/install.sh
@@ -20,8 +20,8 @@ if [ $TRAVIS_OS_NAME = linux ]; then
 
     curl -OL https://github.com/google/protobuf/releases/download/v3.2.0/protoc-3.2.0-linux-x86_64.zip
     unzip protoc-3.2.0-linux-x86_64.zip -d protoc
-    sudo mv protoc3/bin/* /usr/local/bin/
-    sudo mv protoc3/include/* /usr/local/include/
+    sudo mv protoc/bin/* /usr/local/bin/
+    sudo mv protoc/include/* /usr/local/include/
 else
     brew install protobuf
 fi
