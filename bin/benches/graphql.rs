a154b7413f6692640ccabe6707b54c554cfe465a
Yusuf Simonson <simonson@gmail.com>
2018-05-07 14:09:32 +0700

    Added some explanation of the frontend benchmarks

--- bin/benches/graphql.rs
+++ bin/benches/graphql.rs
@@ -1,3 +1,11 @@
+//! Benchmarks for the graphql frontend. Note that these benchmarks aren't
+//! very realistic:
+//! 1) They avoid network overhead.
+//! 2) But they introduce their own overhead in running through a
+//!    not-performance-oriented faux datastore.
+//! What these benchmarks do provide is /some/ idea of how performance might
+//! be relatively affected by changes.
+
 #![feature(test)]
 
 extern crate common;
