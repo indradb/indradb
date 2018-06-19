9f59a8b1cae9f3b7f16f3efe965af8bad23f40b0
Yusuf Simonson <simonson@gmail.com>
2018-06-19 16:20:35 -0400

    Fixed benchmarks

---
+++ bin/benches/api.rs
@@ -0,0 +1,18 @@
+#![feature(test)]
+
+extern crate common;
+extern crate grpcio;
+#[macro_use]
+extern crate indradb;
+extern crate test;
+
+use common::client_datastore::ClientDatastore;
+use common::server;
+use std::thread::spawn;
+
+const TEST_PORT: u16 = 27616;
+
+full_bench_impl!({
+    spawn(move || server::start(&format!("127.0.0.1:{}", TEST_PORT)));
+    ClientDatastore::new(TEST_PORT)
+});
