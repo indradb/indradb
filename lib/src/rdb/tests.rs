72f9d3696628e08ca461960da3bfa50035091f2c
Yusuf Simonson <simonson@gmail.com>
2018-10-14 23:35:43 -0400

    Move imports into functions to prevent warnings

--- lib/src/rdb/tests.rs
+++ lib/src/rdb/tests.rs
@@ -1,14 +1,14 @@
-use super::RocksdbDatastore;
-use util::generate_random_secret;
-use std::path::Path;
-
 #[test]
 fn should_repair() {
+    use super::RocksdbDatastore;
+    use util::generate_random_secret;
+    use std::path::Path;
+
     // TODO: do not hardcode the temp directory to support non-POSIX
     let path = Path::new("/tmp/test-rdb").join(generate_random_secret(8));
 
     // // Make sure we just initialize the database
-    RocksdbDatastore::new(path.to_str().unwrap(), Some(1));
+    RocksdbDatastore::new(path.to_str().unwrap(), Some(1)).unwrap();
 
     // Now try to repair
     RocksdbDatastore::repair(path.to_str().unwrap(), Some(1)).unwrap();
