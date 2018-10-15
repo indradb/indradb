1f87ea6fa05e2438e3d2832cb140943bebe3072e
Yusuf Simonson <simonson@gmail.com>
2018-10-15 14:52:36 -0400

    Killed unused error-chain links

--- bin/src/common/errors.rs
+++ bin/src/common/errors.rs
@@ -1,27 +1,14 @@
 // TODO: remove this once error-chain fixes it
 #![allow(renamed_and_removed_lints)]
 
-use indradb;
-use serde_json;
-use uuid;
 use std::io;
-use capnp;
 
-// TODO: remove any unused links
 error_chain!{
     types {
         Error, ErrorKind, ResultExt, Result;
     }
 
-    links {
-        TransactionError(indradb::Error, indradb::ErrorKind);
-        ValidationError(indradb::ValidationError, indradb::ValidationErrorKind);
-    }
-
     foreign_links {
-        UuidParseError(uuid::parser::ParseError);
-        JsonError(serde_json::Error);
         IoError(io::Error);
-        CapnpError(capnp::Error);
     }
 }
