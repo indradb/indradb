f98783edcbcb924eb16500b1dde9f69cdba25bb8
Yusuf Simonson <simonson@gmail.com>
2018-11-17 12:01:03 -0500

    Created separate benchmark/tests for bulk load optimized configuration

--- lib/src/rdb/mod.rs
+++ lib/src/rdb/mod.rs
@@ -9,14 +9,34 @@ mod tests;
 
 pub use self::datastore::{RocksdbDatastore, RocksdbTransaction};
 
-#[cfg(feature = "bench-suite")]
-full_bench_impl!({
-    use util::generate_temporary_path;
-    RocksdbDatastore::new(&generate_temporary_path(), Some(1), false).unwrap()
-});
+mod normal_config {
+    #[cfg(feature = "bench-suite")]
+    full_bench_impl!({
+        use super::RocksdbDatastore;
+        use util::generate_temporary_path;
+        RocksdbDatastore::new(&generate_temporary_path(), Some(1), false).unwrap()
+    });
 
-#[cfg(feature = "test-suite")]
-full_test_impl!({
-    use util::generate_temporary_path;
-    RocksdbDatastore::new(&generate_temporary_path(), Some(1), false).unwrap()
-});
+    #[cfg(feature = "test-suite")]
+    full_test_impl!({
+        use super::RocksdbDatastore;
+        use util::generate_temporary_path;
+        RocksdbDatastore::new(&generate_temporary_path(), Some(1), false).unwrap()
+    });
+}
+
+mod bulk_load_optimized_config {
+    #[cfg(feature = "bench-suite")]
+    full_bench_impl!({
+        use super::RocksdbDatastore;
+        use util::generate_temporary_path;
+        RocksdbDatastore::new(&generate_temporary_path(), Some(1), true).unwrap()
+    });
+
+    #[cfg(feature = "test-suite")]
+    full_test_impl!({
+        use super::RocksdbDatastore;
+        use util::generate_temporary_path;
+        RocksdbDatastore::new(&generate_temporary_path(), Some(1), true).unwrap()
+    });
+}
