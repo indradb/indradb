bf71a06a3c563f6fd28019b32d43ff4b52f9c3e7
Yusuf Simonson <simonson@gmail.com>
2019-01-12 14:15:13 -0800

    Updated benchmarks to use shutdown handling

--- bin/benches/api.rs
+++ bin/benches/api.rs
@@ -6,13 +6,9 @@ extern crate indradb;
 extern crate test;
 
 use common::client_datastore::ClientDatastore;
-use common::server;
-use std::thread::spawn;
 
 const TEST_PORT: u16 = 27616;
-const WORKER_COUNT: usize = 8;
 
 full_bench_impl!({
-    spawn(move || server::start(&format!("127.0.0.1:{}", TEST_PORT), "memory://", WORKER_COUNT));
-    ClientDatastore::new(TEST_PORT)
+    ClientDatastore::new(TEST_PORT, "memory://".to_string())
 });
