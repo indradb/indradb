858b0aa800fbe830d384f560769865f90a8039e5
Yusuf Simonson <simonson@gmail.com>
2019-12-13 19:48:24 -0500

    Fixed benches

--- bin/benches/api.rs
+++ bin/benches/api.rs
@@ -5,14 +5,20 @@ extern crate common;
 extern crate indradb;
 extern crate test;
 
-use common::client_datastore::ClientDatastore;
-use common::server;
-use std::thread::spawn;
-
 const TEST_PORT: u16 = 27616;
-const WORKER_COUNT: usize = 8;
 
 full_bench_impl!({
-    spawn(move || server::start(&format!("127.0.0.1:{}", TEST_PORT), "memory://", WORKER_COUNT));
-    ClientDatastore::new(TEST_PORT)
+    use std::net::ToSocketAddrs;
+    use common::client_datastore::ClientDatastore;
+    use common::server;
+    use futures::prelude::*;
+    use futures::executor::LocalPool;
+    use futures::task::LocalSpawn;
+
+    let addr = format!("127.0.0.1:{}", TEST_PORT).to_socket_addrs().unwrap().next().unwrap();
+    let exec = LocalPool::new();
+    let spawner = exec.spawner();
+    let f = server::run(addr, indradb::MemoryDatastore::default(), exec.spawner());
+    spawner.spawn_local_obj(Box::pin(f.map_err(|err| panic!(err)).map(|_|())).into()).unwrap();
+    ClientDatastore::new(TEST_PORT, exec)
 });
