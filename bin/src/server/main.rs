f3ffb569cf42620fbb4445e45caf8ebce7957935
Yusuf Simonson <simonson@gmail.com>
2019-12-14 14:23:43 -0500

    Remove the recursion limits

--- bin/src/server/main.rs
+++ bin/src/server/main.rs
@@ -1,5 +1,3 @@
-#![recursion_limit = "1024"]
-
 #[macro_use]
 extern crate failure;
 
--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -6,8 +6,6 @@
 //! use cases, you can use the application, which exposes an API and scripting
 //! layer.
 
-// Used for error-chain, which can recurse deeply
-#![recursion_limit = "1024"]
 #![cfg_attr(feature = "bench-suite", feature(test))]
 
 #[cfg(feature = "bench-suite")]
