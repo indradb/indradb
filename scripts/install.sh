3db5a2eb48d02857c31501a533702bd06b30bce7
Yusuf Simonson <simonson@gmail.com>
2020-02-06 08:55:04 -0500

    Copy capnp assets over

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,12 +5,14 @@ set -ex
 mkdir -p $HOME/bin
 
 if [ ! -f $HOME/bin/capnp ] ; then
+    mkdir -p $HOME/capnp
     curl -O https://capnproto.org/capnproto-c++-0.6.1.tar.gz
     tar zxf capnproto-c++-0.6.1.tar.gz
     cd capnproto-c++-0.6.1
-    ./configure --prefix=$HOME/bin
+    ./configure --prefix=$HOME/capnp
     make -j6 check
     sudo make install
+    cp $HOME/capnp/bin/* $HOME/bin/
 fi
 
 ls -l $HOME/bin
