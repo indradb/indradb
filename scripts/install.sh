d70dd94637587ebd6cd5525f0b62a6c1e59ef5fc
Yusuf Simonson <simonson@gmail.com>
2020-02-12 23:16:31 -0500

    Revert "Copy capnp assets over"
    This reverts commit 3db5a2eb48d02857c31501a533702bd06b30bce7.

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,14 +5,12 @@ set -ex
 mkdir -p $HOME/bin
 
 if [ ! -f $HOME/bin/capnp ] ; then
-    mkdir -p $HOME/capnp
     curl -O https://capnproto.org/capnproto-c++-0.6.1.tar.gz
     tar zxf capnproto-c++-0.6.1.tar.gz
     cd capnproto-c++-0.6.1
-    ./configure --prefix=$HOME/capnp
+    ./configure --prefix=$HOME/bin
     make -j6 check
     sudo make install
-    cp $HOME/capnp/bin/* $HOME/bin/
 fi
 
 ls -l $HOME/bin
