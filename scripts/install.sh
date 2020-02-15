4918bc7586649ef60ce3f368550af06429f056a0
Yusuf Simonson <simonson@gmail.com>
2020-02-15 16:49:51 -0500

    Try upgrading to capnp 0.7

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,10 +6,10 @@ mkdir -p $HOME/bin
 
 if [ ! -f $HOME/bin/capnp ] ; then
     mkdir -p $HOME/capnp
-    curl -O https://capnproto.org/capnproto-c++-0.6.1.tar.gz
-    tar zxf capnproto-c++-0.6.1.tar.gz
-    cd capnproto-c++-0.6.1
-    ./configure --prefix=$HOME/capnp
+    curl -O https://capnproto.org/capnproto-c++-0.7.0.tar.gz
+    tar zxf capnproto-c++-0.7.0.tar.gz
+    cd capnproto-c++-0.7.0
+    ./configure
     make -j6 check
     sudo make install
     cp $HOME/capnp/bin/* $HOME/bin/
