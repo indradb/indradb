19c2dad5f5d209325040acbf214f902c9f6f2326
Yusuf Simonson <simonson@gmail.com>
2020-03-05 21:30:18 -0500

    Fix typo

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,7 +4,7 @@ cargo build
 cargo test
 cargo fmt -- --check
 
-if [ "$TRAVIS_OS_NAME" == "linux" ] && [ "TRAVIS_RUST_VERSION" == "nightly" ]; then
+if [ "$TRAVIS_OS_NAME" == "linux" ] && [ "$TRAVIS_RUST_VERSION" == "nightly" ]; then
     zip -0 ccov.zip `find . \( -name "indradb*.gc*" \) -print`
     ./grcov ccov.zip -s . -t lcov --llvm --branch --ignore-not-existing --ignore "/*" -o lcov.info
     bash <(curl -s https://codecov.io/bash) -f lcov.info
