7418991d2286375f7b24d8830705adf0f867bfda
Yusuf Simonson <simonson@gmail.com>
2020-03-07 18:23:34 -0500

    Fix path

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,6 +6,6 @@ cargo fmt -- --check
 
 if [ "$TRAVIS_OS_NAME" == "linux" ] && [ "$TRAVIS_RUST_VERSION" == "nightly" ]; then
     zip -0 ccov.zip `find . \( -name "indradb*.gc*" \) -print`
-    ./grcov ccov.zip -s . -t lcov --llvm --branch --ignore-not-existing --ignore "/*" -o lcov.info
+    grcov ccov.zip -s . -t lcov --llvm --branch --ignore-not-existing --ignore "/*" -o lcov.info
     bash <(curl -s https://codecov.io/bash) -f lcov.info
 fi
