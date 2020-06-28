fb81bc6a0ea70464cba29adda2f01002812c78ac
Jesper Steen Møller <jesper@selskabet.org>
2020-06-28 18:13:55 +0200

    FInally add the tests for Sled

--- lib/src/sledds/mod.rs
+++ lib/src/sledds/mod.rs
@@ -8,3 +8,16 @@ mod tests;
 
 pub use self::datastore::{SledDatastore, SledTransaction};
 
+#[cfg(feature = "bench-suite")]
+full_bench_impl!({
+    use super::SledDatastore;
+    use crate::util::generate_temporary_path;
+    SledDatastore::new(&generate_temporary_path()).unwrap()
+});
+
+#[cfg(feature = "test-suite")]
+full_test_impl!({
+    use super::SledDatastore;
+    use crate::util::generate_temporary_path;
+    SledDatastore::new(&generate_temporary_path()).unwrap()
+});
