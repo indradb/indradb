81e188d133bd74f0a8a6017347ecbd682ccee062
Yusuf Simonson <simonson@gmail.com>
2020-07-12 17:19:22 -0400

    Test for sled datastore in bin

--- bin/src/common/tests.rs
+++ bin/src/common/tests.rs
@@ -44,3 +44,24 @@ fn should_create_rocksdb_datastore() {
     let count = trans.get_vertex_count().unwrap();
     assert_eq!(count, 0);
 }
+
+#[test]
+fn should_create_sled_datastore() {
+    let mut exec = LocalPool::new();
+
+    let addr = "127.0.0.1:0".to_socket_addrs().unwrap().next().unwrap();
+    let listener = exec.run_until(async { TcpListener::bind(&addr).await }).unwrap();
+    let port = listener.local_addr().unwrap().port();
+
+    let datastore = indradb::SledDatastore::new(&generate_temporary_path()).unwrap();
+    let f = server::run(listener, datastore, exec.spawner());
+    exec.spawner()
+        .spawn_local_obj(Box::pin(f.map_err(|err| panic!(err)).map(|_| ())).into())
+        .unwrap();
+
+    // Just make sure we can run a command
+    let datastore = ClientDatastore::new(port as u16, exec);
+    let trans = datastore.transaction().unwrap();
+    let count = trans.get_vertex_count().unwrap();
+    assert_eq!(count, 0);
+}
