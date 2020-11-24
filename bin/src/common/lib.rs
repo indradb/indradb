e0d7126d3ccb6ca950e46de5d3129dbc10ea7722
Yusuf Simonson <simonson@gmail.com>
2020-11-24 16:21:03 -0500

    Separate out proto interface

--- bin/src/common/lib.rs
+++ bin/src/common/lib.rs
@@ -1,19 +1,13 @@
 #[macro_use]
 extern crate capnp_rpc;
 #[cfg(test)]
-#[cfg(test)]
 #[macro_use]
 extern crate indradb;
 #[cfg(not(test))]
 extern crate indradb;
 
-pub mod autogen;
-#[macro_use]
-pub mod converters;
 pub mod client_datastore;
 pub mod server;
 
 #[cfg(test)]
 mod tests;
-
-pub use crate::client_datastore::ClientDatastore;
