d233073b04d86ba14eac46313cd513097607b12f
Yusuf Simonson <simonson@gmail.com>
2020-11-29 09:50:07 -0500

    Disable codecov for now

--- scripts/test.sh
+++ scripts/test.sh
@@ -10,5 +10,7 @@ fi
 if [ "$TRAVIS_OS_NAME" == "linux" ] && [ "$TRAVIS_RUST_VERSION" == "nightly" ]; then
     zip -0 ccov.zip `find . \( -name "indradb*.gc*" \) -print`
     grcov ccov.zip -s . -t lcov --llvm --branch --ignore-not-existing --ignore "/*" -o lcov.info
-    bash <(curl -s https://codecov.io/bash) -f lcov.info
+    # TODO: re-enable codecov when we can figure out how to make the coverage
+    # analysis more accurate
+    # bash <(curl -s https://codecov.io/bash) -f lcov.info
 fi
