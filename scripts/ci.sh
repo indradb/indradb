baf5a702f933b96f5094f2a148e5634ef2a3e2c4
Yusuf Simonson <simonson@gmail.com>
2020-12-20 15:34:04 -0500

    Be mindful of cwd

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -9,12 +9,15 @@ echo "running ch.sh with rust_variant=$rust_variant, os=$os"
 mkdir -p $HOME/cached-deps/bin
 
 if [ ! -f $HOME/cached-deps/bin/capnp ] ; then
-    curl -O https://capnproto.org/capnproto-c++-0.6.1.tar.gz
-    tar zxf capnproto-c++-0.6.1.tar.gz
-    cd capnproto-c++-0.6.1
-    ./configure --prefix=$HOME/cached-deps
-    make -j6 check
-    sudo make install
+    pushd ~
+        curl -O https://capnproto.org/capnproto-c++-0.6.1.tar.gz
+        tar zxf capnproto-c++-0.6.1.tar.gz
+        pushd capnproto-c++-0.6.1
+            ./configure --prefix=$HOME/cached-deps
+            make -j6 check
+            sudo make install
+        popd
+    popd
 fi
 
 PATH="${HOME}/.local/bin:${HOME}/cached-deps/bin:$PATH"
@@ -26,8 +29,14 @@ if [ "$os" == "linux" ]; then
         cargo clippy
         cargo fmt -- --check
     else
-        (cd lib && cargo check --features=bench-suite)
-        (cd lib/fuzz && cargo check)
-        (cd bin && cargo check --features=bench-suite)
+        pushd lib
+            cargo check --features=bench-suite
+            pushd fuzz
+                cargo check
+            popd
+        popd
+        pushd bin
+            cargo check --features=bench-suite
+        popd
     fi
 fi
