e2b6b117d5f54d9dd47ddccc655278976b67eac8
Yusuf Simonson <simonson@gmail.com>
2020-12-20 19:33:00 -0500

    See if apt installing capn proto works

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -4,23 +4,8 @@ set -ex
 
 rust_variant=$1
 os=$2
-echo "running ch.sh with rust_variant=$rust_variant, os=$os"
 
-mkdir -p $HOME/cached-deps/bin
-
-if [ ! -f $HOME/cached-deps/bin/capnp ] ; then
-    pushd ~
-        curl -O https://capnproto.org/capnproto-c++-0.6.1.tar.gz
-        tar zxf capnproto-c++-0.6.1.tar.gz
-        pushd capnproto-c++-0.6.1
-            ./configure --prefix=$HOME/cached-deps
-            make -j6 check
-            sudo make install
-        popd
-    popd
-fi
-
-PATH="${HOME}/.local/bin:${HOME}/cached-deps/bin:$PATH"
+apt-get install capnproto
 
 make test
 
