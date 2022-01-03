eb15c095c17bb046398653e88901809ee8be0c40
Yusuf Simonson <simonson@gmail.com>
2022-01-03 11:37:22 -0500

    Support input arguments

--- plugins/centrality/src/lib.rs
+++ plugins/centrality/src/lib.rs
@@ -99,15 +99,13 @@ impl plugin::Plugin for CentralityPlugin {
         arg: serde_json::Value,
     ) -> Result<serde_json::Value, plugin::Error> {
         let vertex_count = datastore.get_vertex_count()?;
-
-        let t_filter = arg
-            .get("t_filter")
-            .map(|t_filter| indradb::Identifier::new(t_filter.as_str().unwrap()).unwrap());
-        // TODO: pull max iterations and max delta as configs
+        let t_filter = parse_t_filter(&arg)?;
+        let max_iterations = parse_max_iterations(&arg)?;
+        let max_delta = parse_max_delta(&arg)?;
 
         let mut prev_centrality_map = BTreeMap::default();
         let mut deltas = Vec::new();
-        for _ in 0..DEFAULT_MAX_ITERATIONS {
+        for _ in 0..max_iterations {
             let mut mapper = Arc::new(CentralityMapper::new(
                 datastore.clone(),
                 prev_centrality_map.clone(),
@@ -121,7 +119,7 @@ impl plugin::Plugin for CentralityPlugin {
                 delta += f64::abs(centrality - prev_centrality);
             }
             delta /= vertex_count as f64;
-            if delta < DEFAULT_MAX_DELTA {
+            if delta < max_delta {
                 return Ok(delta.into());
             }
             prev_centrality_map = cur_centrality_map;
@@ -131,10 +129,45 @@ impl plugin::Plugin for CentralityPlugin {
         // TODO: persist
 
         Err(plugin::Error::Other(Box::new(DidNotConvergeError {
-            target_delta: DEFAULT_MAX_DELTA,
-            iterations: DEFAULT_MAX_ITERATIONS,
+            target_delta: max_delta,
+            iterations: max_iterations,
             deltas,
         })))
+    }
+}
+
+fn parse_t_filter(arg: &serde_json::Value) -> Result<Option<indradb::Identifier>, plugin::Error> {
+    if let Some(value) = arg.get("t_filter") {
+        if let serde_json::Value::String(s) = value {
+            let ident = indradb::Identifier::new(s).map_err(|err| plugin::Error::Other(Box::new(err)))?;
+            return Ok(Some(ident));
+        }
+        Err(plugin::Error::InvalidArgument("'t_filter' is not a string".to_string()))
+    } else {
+        Ok(None)
+    }
+}
+
+fn parse_max_iterations(arg: &serde_json::Value) -> Result<u16, plugin::Error> {
+    if let Some(value) = arg.get("max_iterations") {
+        if let serde_json::Value::Number(num) = value {
+            if let Some(num_u64) = num.as_u64() {
+                if num_u64 < u16::max_value() as u64 {
+                    return Ok(num_u64 as u16);
+                }
+            }
+        }
+        Err(plugin::Error::InvalidArgument("'max_iterations' is not a u16".to_string()))
+    } else {
+        Ok(DEFAULT_MAX_ITERATIONS)
+    }
+}
+
+fn parse_max_delta(arg: &serde_json::Value) -> Result<f64, plugin::Error> {
+    match arg.get("max_delta") {
+        Some(value) if value.is_f64() => Ok(value.as_f64().unwrap()),
+        Some(_) => Err(plugin::Error::InvalidArgument("'max_delta' is not an f64".to_string())),
+        None => Ok(DEFAULT_MAX_DELTA)
     }
 }
 
