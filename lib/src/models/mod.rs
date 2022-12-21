79365f36ec288f4e5c89c7abae0f898ff62c4c23
ysimonson <simonson@gmail.com>
2022-12-21 18:49:22 -0500

    remove compatibility layer

--- lib/src/models/mod.rs
+++ lib/src/models/mod.rs
@@ -2,7 +2,6 @@ mod bulk_insert;
 mod edges;
 mod identifiers;
 mod json;
-mod properties;
 mod queries;
 mod vertices;
 
@@ -10,6 +9,5 @@ pub use self::bulk_insert::BulkInsertItem;
 pub use self::edges::{Edge, EdgeKey};
 pub use self::identifiers::Identifier;
 pub use self::json::Json;
-pub use self::properties::{EdgeProperties, EdgeProperty, NamedProperty, VertexProperties, VertexProperty};
 pub use self::queries::*;
 pub use self::vertices::Vertex;
