b79153210a274477604ff8ee7a83025137d73468
ysimonson <simonson@gmail.com>
2022-12-21 19:27:24 -0500

    Revert "remove compatibility layer"
    This reverts commit 79365f36ec288f4e5c89c7abae0f898ff62c4c23.

---
+++ lib/src/models/properties.rs
@@ -0,0 +1,106 @@
+use crate::{Edge, EdgeKey, Identifier, Vertex};
+
+use uuid::Uuid;
+
+/// Represents a vertex property.
+#[derive(Clone, Debug, PartialEq)]
+pub struct VertexProperty {
+    /// The id of the vertex.
+    pub id: Uuid,
+
+    /// The property value.
+    pub value: serde_json::Value,
+}
+
+impl VertexProperty {
+    /// Creates a new vertex property.
+    ///
+    /// # Arguments
+    /// * `id`: The id of the vertex.
+    /// * `value`: The property value.
+    pub fn new(id: Uuid, value: serde_json::Value) -> Self {
+        Self { id, value }
+    }
+}
+
+/// A property.
+#[derive(Clone, Debug, PartialEq)]
+pub struct NamedProperty {
+    /// The id of the vertex.
+    pub name: Identifier,
+
+    /// The property value.
+    pub value: serde_json::Value,
+}
+
+impl NamedProperty {
+    /// Creates a new vertex property.
+    ///
+    /// # Arguments
+    /// * `name`: The name of the property.
+    /// * `value`: The property value.
+    pub fn new(name: Identifier, value: serde_json::Value) -> Self {
+        Self { name, value }
+    }
+}
+
+/// A vertex with properties.
+#[derive(Clone, Debug, PartialEq)]
+pub struct VertexProperties {
+    /// The vertex.
+    pub vertex: Vertex,
+    /// All of the vertex's properties.
+    pub props: Vec<NamedProperty>,
+}
+
+impl VertexProperties {
+    /// Creates new properties for a given vertex.
+    ///
+    /// # Arguments
+    /// * `vertex`: The vertex information
+    /// * `props`: The properties
+    pub fn new(vertex: Vertex, props: Vec<NamedProperty>) -> Self {
+        VertexProperties { vertex, props }
+    }
+}
+
+/// An edge with properties.
+#[derive(Clone, Debug, PartialEq)]
+pub struct EdgeProperties {
+    /// The edge.
+    pub edge: Edge,
+    /// All of the edge's properties.
+    pub props: Vec<NamedProperty>,
+}
+
+impl EdgeProperties {
+    /// Creates a new edge properties for a given edge.
+    ///
+    /// # Arguments
+    /// * `edge`: The edge information
+    /// * `props`: The properties
+    pub fn new(edge: Edge, props: Vec<NamedProperty>) -> Self {
+        EdgeProperties { edge, props }
+    }
+}
+
+/// Represents an edge property.
+#[derive(Clone, Debug, PartialEq)]
+pub struct EdgeProperty {
+    /// The key to the edge.
+    pub key: EdgeKey,
+
+    /// The property value.
+    pub value: serde_json::Value,
+}
+
+impl EdgeProperty {
+    /// Creates a new edge property.
+    ///
+    /// # Arguments
+    /// * `key`: The key to the edge.
+    /// * `value`: The property value.
+    pub fn new(key: EdgeKey, value: serde_json::Value) -> Self {
+        Self { key, value }
+    }
+}
