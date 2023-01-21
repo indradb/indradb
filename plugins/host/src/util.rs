c9b46e7e4ea1b38325e8ec75886b0f0358ab8069
ysimonson <simonson@gmail.com>
2023-01-20 23:21:37 -0500

    checkpoint

--- plugins/host/src/util.rs
+++ plugins/host/src/util.rs
@@ -23,37 +23,46 @@ pub trait VertexMapper: Send + Sync + 'static {
 /// * `mapper`: Specified options and the map operation to run.
 /// * `database`: The database.
 pub fn map<'a, M: VertexMapper>(txn: &(dyn indradb::Transaction<'a> + 'a), mapper: Arc<M>) -> Result<(), Error> {
-    let last_err: Arc<Mutex<Option<Error>>> = Arc::new(Mutex::new(None));
+    let first_err: Arc<Mutex<Option<Error>>> = Arc::new(Mutex::new(None));
     let pool = ThreadPool::new(max(mapper.num_threads(), 1));
     let txn_ptr = txn as *const dyn indradb::Transaction<'a>;
+    let mut i = 0;
 
     for vertex in unsafe { (*txn_ptr).all_vertices()? } {
-        if last_err.lock().unwrap().is_some() {
+        i += 1;
+        if i % 1000 == 0 && first_err.lock().unwrap().is_some() {
+            // Break on error, but also only check every once in a while since
+            // the error is behind a mutex.
             break;
         }
         match vertex {
             Ok(vertex) => {
                 let vertex: indradb::Vertex = vertex;
                 let mapper = mapper.clone();
-                let last_err = last_err.clone();
+                let first_err = first_err.clone();
                 pool.execute(move || {
                     if let Err(err) = mapper.map(vertex) {
-                        *last_err.lock().unwrap() = Some(err);
+                        let mut first_err = first_err.lock().unwrap();
+                        if first_err.is_none() {
+                            *first_err = Some(err);
+                        }
                     }
                 });
             }
             Err(err) => {
-                *last_err.lock().unwrap() = Some(Error::IndraDB(err));
-                break;
+                let mut first_err = first_err.lock().unwrap();
+                if first_err.is_none() {
+                    *first_err = Some(Error::IndraDB(err));
+                }
             }
         }
     }
 
     pool.join();
 
-    let mut last_err = last_err.lock().unwrap();
-    if last_err.is_some() {
-        Err(last_err.take().unwrap())
+    let mut first_err = first_err.lock().unwrap();
+    if first_err.is_some() {
+        Err(first_err.take().unwrap())
     } else {
         Ok(())
     }
